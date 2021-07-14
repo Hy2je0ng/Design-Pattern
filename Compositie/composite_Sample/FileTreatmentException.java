75236e6b7f2f5c53173ae9c7e64a32300f489419
hy2je0ng <hy2je0ng@naver.com>
2021-07-14 20:26:04 +0900

    [upload] Composite

---
+++ Compositie/composite_Sample/FileTreatmentException.java
@@ -0,0 +1,10 @@
+package composite_Sample;
+
+public class FileTreatmentException extends RuntimeException {
+	public FileTreatmentException() {
+	}
+
+	public FileTreatmentException(String msg) {
+		super(msg);
+	}
+}
